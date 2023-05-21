ad8cbe70fc61bbbfdab5762a27f630387e8b0cdd
nolanmatt34 <nolan.matt@northeastern.edu>
2023-05-20 23:19:33 -0400

    Changing HomePage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,15 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Medford Mustangs</Title>
+      <h2>Team Roster</h2>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
+        <a href="https://www.linkedin.com/in/matthew-nolan-91548b114/" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to learn more about the developer
       </p>
     </main>
   );
